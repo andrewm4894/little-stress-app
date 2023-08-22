8d4cf2342a4d63719edfa0ea213ca46fdd061307
Andrew Maguire <andrewm4894@gmail.com>
2023-08-22 18:01:01 +0100

    dev

--- little-stress-app.py
+++ little-stress-app.py
@@ -1,80 +1,71 @@
 import tkinter as tk
-from tkinter import ttk, tix
+from tkinter import ttk, IntVar
+from PIL import Image, ImageTk
+import os
 import subprocess
+from tkinter import simpledialog, messagebox
 
-def run_command(cmd_template, duration_var):
-    cmd = cmd_template.format(duration_var.get())
-    subprocess.Popen(cmd, shell=True)
+def run_command(command_template, duration_var):
+    duration = duration_var.get()
+    cmd = command_template.format(duration)
+    try:
+        subprocess.Popen(cmd, shell=True)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
 
-def main():
-    root = tix.Tk()
-    root.title("Stress-ng Tester")
+root = tk.Tk()
+root.title("Stress-ng Command Runner")
 
-    balloon = tix.Balloon(root)
+# Set duration frame
+duration_frame = ttk.LabelFrame(root, text="Set Duration (seconds)", padding="10")
+duration_frame.pack(padx=10, pady=5, fill="x", expand=True)
 
-    # Duration input
-    ttk.Label(root, text="Duration (seconds):").pack(pady=5)
-    duration_var = tk.IntVar(value=15)  # default value set to 15 seconds
-    ttk.Entry(root, textvariable=duration_var).pack(pady=5)
+duration_var = IntVar(value=15)  # Default duration set to 15 seconds
+duration_spinbox = ttk.Spinbox(duration_frame, from_=1, to=3600, textvariable=duration_var, width=5)
+duration_spinbox.pack(pady=5)
 
-    # Define commands with their emojis
-    commands = {
-        "🔥 CPU Load": "stress-ng --cpu 4 --timeout {}s",
-        "🧠 Memory Load": "stress-ng --vm 2 --vm-bytes 256M --timeout {}s",
-        "💽 I/O Load": "stress-ng --io 4 --timeout {}s",
-        "🍴 Fork/Process Load": "stress-ng --fork 4 --timeout {}s",
-        "🌐 Network Load": "stress-ng --netdev 2 --timeout {}s",
-        "🔢 Matrix Load": "stress-ng --matrix 2 --timeout {}s",
-        "💾 Disk Load": "stress-ng --hdd 2 --timeout {}s",
-        "🔄 Context Switching Load": "stress-ng --context 2 --timeout {}s",
-        "💰 Cache Thrashing": "stress-ng --cache 2 --cache-ops 1000000 --timeout {}s",
-        "🔋 Thermal and Power": "stress-ng --power 2 --timeout {}s",
-        "🔒 Semaphore Stress": "stress-ng --sem 4 --timeout {}s",
-        "🧟 Zombie Processes": "stress-ng --zombie 1000 --timeout {}s",
-        "🔌 Socket Load": "stress-ng --sock 2 --timeout {}s",
-        "✉️ Msg Stress": "stress-ng --msg 2 --timeout {}s",
-        "🌪 Combination Load": "stress-ng --cpu 2 --io 2 --vm 2 --vm-bytes 128M --timeout {}s"
-    }
+# Commands and their explanations
+commands = {
+    "cpu_load.jpeg": "stress-ng --cpu 4 --timeout {}s",
+    "memory_load.png": "stress-ng --vm 2 --vm-bytes 256M --timeout {}s",
+    "io_load.png": "stress-ng --io 4 --timeout {}s",
+    "hdd_load.png": "stress-ng --hdd 2 --timeout {}s",
+    "socket_load.png": "stress-ng --sock 6 --timeout {}s",
+    "matrix_load.png": "stress-ng --matrix 2 --timeout {}s",
+}
 
-    explanations = {
-        "🔥 CPU Load": "Stresses the CPU cores.",
-        "🧠 Memory Load": "Stresses the RAM.",
-        "💽 I/O Load": "Stresses the I/O operations.",
-        "🍴 Fork/Process Load": "Stresses the system by constantly forking processes.",
-        "🌐 Network Load": "Stresses the network interfaces.",
-        "🔢 Matrix Load": "Performs matrix multiplications to stress the CPU.",
-        "💾 Disk Load": "Stresses disk writes.",
-        "🔄 Context Switching Load": "Stresses the context switching mechanism.",
-        "💰 Cache Thrashing": "Stresses the cache.",
-        "🔋 Thermal and Power": "Stress power management and thermal code.",
-        "🔒 Semaphore Stress": "Stresses system semaphores.",
-        "🧟 Zombie Processes": "Creates a large number of zombie processes.",
-        "🔌 Socket Load": "Stresses socket operations.",
-        "✉️ Msg Stress": "Stresses System V message passing.",
-        "🌪 Combination Load": "Combines multiple scenarios together for a composite load."
-    }
+explanations = {
+    "cpu_load": "This stresses the CPU with 4 workers for a specified duration.",
+    "memory_load": "This stresses the virtual memory with 2 workers and 256M bytes per worker for a specified duration.",
+    "io_load": "Stresses the I/O by spawning 4 workers.",
+    "hdd_load": "Stresses the hard drive by spawning 2 workers.",
+    "socket_load": "Stresses sockets by creating 6 workers.",
+    "matrix_load": "Stresses CPU by performing matrix multiplications with 2 workers.",
+}
 
-    # Frame for the buttons, so we can use grid inside this frame
-    button_frame = ttk.Frame(root)
-    button_frame.pack(pady=10)
+# Button frame
+button_frame = ttk.LabelFrame(root, text="Stress Commands", padding="10")
+button_frame.pack(padx=10, pady=5, fill="x", expand=True)
 
-    # Create and place buttons in two columns within button_frame
-    row = 0
-    col = 0
-    for label, cmd_template in commands.items():
-        btn_font = ("Segoe UI Emoji", 10)
-        btn = tk.Button(button_frame, text=label, command=lambda cmd_template=cmd_template: run_command(cmd_template, duration_var), font=btn_font)
-        btn.grid(row=row, column=col, pady=10, padx=5, sticky='w'+'e')
-        balloon.bind_widget(btn, msg="Command: " + cmd_template.format("[duration]") + "\nExplanation: " + explanations[label])
+# Create and place buttons in two columns within button_frame
+row = 0
+col = 0
+for image_file, cmd_template in commands.items():
+    # Load the image
+    image_path = os.path.join(os.path.dirname(__file__), image_file)
+    image = Image.open(image_path)
+    photo = ImageTk.PhotoImage(image)
 
-        # Logic for 2 column layout
-        if col == 0:
-            col = 1
-        else:
-            col = 0
-            row += 1
+    # Create a button with the image
+    btn = tk.Button(button_frame, image=photo, command=lambda cmd_template=cmd_template: run_command(cmd_template, duration_var))
+    btn.image = photo  # Keep a reference to prevent garbage collection
+    btn.grid(row=row, column=col, pady=10, padx=5, sticky='w'+'e')
 
-    root.mainloop()
+    # Logic for 2 column layout
+    if col == 0:
+        col = 1
+    else:
+        col = 0
+        row += 1
 
-if __name__ == "__main__":
-    main()
+root.mainloop()
