ad4b90560f999a2259832c8cf1d3d8a199240930
Andrew Maguire <andrewm4894@gmail.com>
2023-08-22 17:37:38 +0100

    dev

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # little-stress-app
+
+ChatGPT wrote this.
+
+Little demo app to run some stress-ng commands.
--- little-stress-app.py
+++ little-stress-app.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk, tix
-
 import subprocess
 
 def run_command(cmd_template, duration_var):
@@ -18,20 +17,46 @@ def main():
     duration_var = tk.IntVar(value=60)  # default value
     ttk.Entry(root, textvariable=duration_var).pack(pady=5)
 
-    # Define commands
+    # Define commands with their emojis
     commands = {
-        # ... (Same commands as before) ...
+        "🔥 CPU Load": "stress-ng --cpu 4 --timeout {}s",
+        "🧠 Memory Load": "stress-ng --vm 2 --vm-bytes 256M --timeout {}s",
+        "💽 I/O Load": "stress-ng --io 4 --timeout {}s",
+        "🍴 Fork/Process Load": "stress-ng --fork 4 --timeout {}s",
+        "🌐 Network Load": "stress-ng --netdev 2 --timeout {}s",
+        "🔢 Matrix Load": "stress-ng --matrix 2 --timeout {}s",
+        "💾 Disk Load": "stress-ng --hdd 2 --timeout {}s",
+        "🔄 Context Switching Load": "stress-ng --context 2 --timeout {}s",
+        "💰 Cache Thrashing": "stress-ng --cache 2 --cache-ops 1000000 --timeout {}s",
+        "🔋 Thermal and Power": "stress-ng --power 2 --timeout {}s",
+        "🔒 Semaphore Stress": "stress-ng --sem 4 --timeout {}s",
+        "🧟 Zombie Processes": "stress-ng --zombie 1000 --timeout {}s",
+        "🔌 Socket Load": "stress-ng --sock 2 --timeout {}s",
+        "✉️ Msg Stress": "stress-ng --msg 2 --timeout {}s",
+        "🌪 Combination Load": "stress-ng --cpu 2 --io 2 --vm 2 --vm-bytes 128M --timeout {}s"
     }
 
     explanations = {
-        "CPU Load": "Stresses the CPU cores.",
-        "Memory Load": "Stresses the RAM.",
-        # ... Add explanations for all other commands ...
+        "🔥 CPU Load": "Stresses the CPU cores.",
+        "🧠 Memory Load": "Stresses the RAM.",
+        "💽 I/O Load": "Stresses the I/O operations.",
+        "🍴 Fork/Process Load": "Stresses the system by constantly forking processes.",
+        "🌐 Network Load": "Stresses the network interfaces.",
+        "🔢 Matrix Load": "Performs matrix multiplications to stress the CPU.",
+        "💾 Disk Load": "Stresses disk writes.",
+        "🔄 Context Switching Load": "Stresses the context switching mechanism.",
+        "💰 Cache Thrashing": "Stresses the cache.",
+        "🔋 Thermal and Power": "Stress power management and thermal code.",
+        "🔒 Semaphore Stress": "Stresses system semaphores.",
+        "🧟 Zombie Processes": "Creates a large number of zombie processes.",
+        "🔌 Socket Load": "Stresses socket operations.",
+        "✉️ Msg Stress": "Stresses System V message passing.",
+        "🌪 Combination Load": "Combines multiple scenarios together for a composite load."
     }
 
     # Create and place buttons
     for label, cmd_template in commands.items():
-        btn = ttk.Button(root, text="▶ " + label, command=lambda cmd_template=cmd_template: run_command(cmd_template, duration_var))
+        btn = ttk.Button(root, text=label, command=lambda cmd_template=cmd_template: run_command(cmd_template, duration_var))
         btn.pack(pady=10)
         balloon.bind_widget(btn, msg="Command: " + cmd_template.format("[duration]") + "\nExplanation: " + explanations[label])
 
